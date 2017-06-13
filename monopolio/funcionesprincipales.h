e95576e40dd167c10652db9593c1d1f007ccc4c5
pebeto <pebeto99@gmail.com>
2017-06-13 18:56:36 -0500

    Update funcionesprincipales.h

--- monopolio/funcionesprincipales.h
+++ monopolio/funcionesprincipales.h
@@ -1,14 +1,18 @@
 #ifndef FUNCIONESPRINCIPALES_H_INCLUDED
 #define FUNCIONESPRINCIPALES_H_INCLUDED
 
-void mover(jugador *g, casillero _juego[]){
-    int movimientos=dado();
-    if(movimientos==-1){
-        carcel(g->posicion);
-        g->carcel=true;
-        g->contadorCarcel++;
+void ingresarDatosJug(int *_nJugadores, jugador *_players){
+    for(int i=0; i<*_nJugadores; i++){
+        cout<<"Ingresa tu nombre: ";
+        cin>>(*(_players+i)).nombre;
     }
 }
 
+void ingresarNJug(int *_nJugadores){
+    do{
+        cout<<"Ingrese el número de jugadores: (2 min. / 6 max.)";
+        cin>>*_nJugadores;
+    }while(*_nJugadores<=1 || *_nJugadores>6);
+}
 
 #endif // FUNCIONESPRINCIPALES_H_INCLUDED
