82ec4c5c5dea189e3d2635dd1d685ee9f045ebc9
pebeto <pebeto99@gmail.com>
2017-06-23 00:54:32 -0500

    Update funcionesprincipales.h

--- monopolio/funcionesprincipales.h
+++ monopolio/funcionesprincipales.h
@@ -5,6 +5,7 @@ void ingresarDatosJug(int *_nJugadores, jugador *_players){
     for(int i=0; i<*_nJugadores; i++){
         cout<<"Ingresa tu nombre: ";
         cin>>(*(_players+i)).nombre;
+        cout<<"Ingresa tu color: "<<endl;
     }
 }
 
@@ -15,25 +16,25 @@ void ingresarNJug(int *_nJugadores){
     }while(*_nJugadores<=1 || *_nJugadores>6);
 }
 
-void movimiento(){
+void inicioPartida(int *_nJugadores, jugador *_players){
+    ingresarNJug(_nJugadores);
+    ingresarDatosJug(_nJugadores, _players);
+}
 
-    int _movimientos=dado();
-    int _posicion=_posicion+_movimientos;
-    switch(_posicion){
-    case 0 : //CASO GO
-
+void mover(jugador *_players, casillero *_juego, int nJugador){
+    int cantidadDeMovimientos=dado();
+    (*(_players+nJugador)).posicion+=cantidadDeMovimientos;
+    if((*(_players+nJugador)).posicion>=40){
+        (*(_players+nJugador)).posicion-=40;
+        (*(_players+nJugador)).nvueltas++;
+        (*(_players+nJugador)).dinero+=200;
+    }
+    switch((*(_players+nJugador)).posicion){
+    case 10 : case 20 ://CASO CARCEL VISITA && PARADA LIBRE
         break;
-    case 10 : //CASO CARCEL VISITA
-
-        break;
-    case 20 : //CASO PARADA LIBRE
-
-        break;
-    case 30 : //CASO CARCEL
-
-        break;
-    case -1 : //CASO CARCEL DADOS
-
+    case 30 : case -1: //CASO CARCEL
+        (*(_players+nJugador)).carcel=true;
+        (*(_players+nJugador)).posicion=10;
         break;
     case 2 : case 17 : case 33 : //CASO ARCA COMUNAL
 
@@ -42,14 +43,39 @@ void movimiento(){
 
         break;
     case 4 : //CASO IMPUESTO CARO
-
+        (*(_players+nJugador)).dinero-=200;
         break;
     case 38 : //CASO IMPUESTO BARATO
-
+        (*(_players+nJugador)).dinero-=75;
         break;
     default :
-
+        int _opcion;
+        cout<<"Opciones de casillero: "<<endl;
+        if((*(_players+nJugador)).nvueltas!=0){
+            cout<<"1. COMPRAR LUGAR."<<endl;
+            cout<<"2. SUBASTAR LUGAR."<<endl;
+            if((*(_players+nJugador)).posesiones[(*(_players+nJugador)).posicion])
+                cout<<"3. VENDER LUGAR."<<endl;
+            cout<<"4. SALTAR TURNO."<<endl;
+            do{
+            cout<<"OPCION: ";cin>>_opcion;
+            }while(_opcion>4 || _opcion<=0);
+        }
         break;
+    }
+}
+
+void salir(){
+    exit(0);
+}
+
+void turno(jugador *_players, casillero *_juego, int *_nJugadores){
+    int nJugador=0;
+    while(nJugador<*_nJugadores){
+
+        nJugador++;
+        if(nJugador==*_nJugadores)
+            nJugador=0;
     }
 }
 
