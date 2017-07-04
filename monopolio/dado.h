5195f2da841309459e94dd68bb9e5af2f4e58be9
pebeto <pebeto99@gmail.com>
2017-07-04 13:08:56 -0500

    Update dado.h

--- monopolio/dado.h
+++ monopolio/dado.h
@@ -7,7 +7,6 @@ int dado(){
     int _valordado1= rand()%6+1;
     if(_valordado==_valordado1){
         while(_valordado==_valordado1){
-            cout<<"Doble "<<_valordado<<": "<<endl;
             casillasamover+=_valordado+_valordado1;
             _valordado= rand()%6+1;
             _valordado1= rand()%6+1;
@@ -15,8 +14,6 @@ int dado(){
         }
     }
     if(intentos<3){
-        cout<<"Dado 1: "<<_valordado<<endl;
-        cout<<"Dado 2: "<<_valordado1<<endl;
         casillasamover+=_valordado+_valordado1;
     }
     else{
