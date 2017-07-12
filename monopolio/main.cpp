7f188e1abb200744a48e1b906d1e389dcd5fdad8
pebeto <pebeto99@gmail.com>
2017-07-11 20:39:15 -0500

    Add files via upload

--- monopolio/main.cpp
+++ monopolio/main.cpp
@@ -5,7 +5,7 @@
 #include "casillero.h"
 #include "jugadores.h"
 #include "dado.h"
-#include "parada.h"
+#include "remplazoarchivo.h"
 #include "funcionesprincipales.h"
 #include "menu.h"
 using namespace std;
@@ -78,12 +78,6 @@ int main(){
     else
         cout<<"no";
 
-    */
-
-    //VARIABLES PRINCIPALES
-    int *nJugadores= new int;
-    srand((unsigned)time(NULL));
-    /*
     casillero *aux=new casillero;
 
     tablero.open(".\\data\\tableroMOD.txt", ios::out | ios::in | ios::binary);
@@ -100,25 +94,21 @@ int main(){
         cerr<<"nel prro";
     }
     */
+
+    //VARIABLES PRINCIPALES
+    int *nJugadores= new int;
+    srand((unsigned)time(NULL));
+
     //COPIA EL ARCHIVO DEL TABLERO ORIGINAL AL MODIFICABLE
-    fstream tablero1;
-    fstream tablero2;
-    casillero *aux=new casillero;
-    tablero1.open(".\\data\\tableroOR.txt", ios::in | ios::binary);
-    tablero2.open(".\\data\\tableroMOD.txt", ios::out | ios::binary);
-    tablero1.read(reinterpret_cast<char*>(aux),sizeof(casillero));
-    while(!tablero1.eof()){
-        tablero2.write(reinterpret_cast<char*>(aux),sizeof(casillero));
-        tablero1.read(reinterpret_cast<char*>(aux),sizeof(casillero));
-    }
-    delete aux;
-    tablero1.close();
-    tablero2.close();
+    //El archivo "tableroOR.txt" de la carpeta "data" se copia
+    //en el "tableroMOD.txt" para la nueva inicializacion de la partida.
+    nuevoArchivo();
 
     //GAME LOOP
     while(true){
         inicioPartida(nJugadores);
     }
+
     delete nJugadores;
     return 0;
 }
---
+++ monopolio/remplazoarchivo.h
@@ -0,0 +1,24 @@
+#ifndef REMPLAZOARCHIVO_H_INCLUDED
+#define REMPLAZOARCHIVO_H_INCLUDED
+using namespace std;
+
+    //COPIA EL ARCHIVO DEL TABLERO ORIGINAL AL MODIFICABLE
+    //El archivo "tableroOR.txt" de la carpeta "data" se copia
+    //en el "tableroMOD.txt" para la nueva inicializacion de la partida.
+void nuevoArchivo(){
+    fstream tablero1;
+    fstream tablero2;
+    casillero *aux=new casillero;
+    tablero1.open(".\\data\\tableroOR.txt", ios::in | ios::binary);
+    tablero2.open(".\\data\\tableroMOD.txt", ios::out | ios::binary);
+    tablero1.read(reinterpret_cast<char*>(aux),sizeof(casillero));
+    while(!tablero1.eof()){
+        tablero2.write(reinterpret_cast<char*>(aux),sizeof(casillero));
+        tablero1.read(reinterpret_cast<char*>(aux),sizeof(casillero));
+    }
+    delete aux;
+    tablero1.close();
+    tablero2.close();
+}
+
+#endif // REMPLAZOARCHIVO_H_INCLUDED
