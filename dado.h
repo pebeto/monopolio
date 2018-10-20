09feceda8ce2a3f2f0624974de88dea3083133dc
pebeto <pebeto@users.noreply.github.com>
2018-10-20 02:11:08 -0500

    ddebug
    Signed-off-by: pebeto <pebeto@users.noreply.github.com>

--- dado.h
+++ dado.h
@@ -1,10 +1,6 @@
-#ifndef DADO_H_INCLUDED
-#define DADO_H_INCLUDED
-
 void dado(int &_dado1, int &_dado2,int &_intentos){
     _dado1= rand()%6+1;
     _dado2= rand()%6+1;
     _intentos++;
 }
 
-#endif // DADO_H_INCLUDED
